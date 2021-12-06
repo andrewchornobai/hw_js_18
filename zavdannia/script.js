67aa538ec8a56e8cc02908021e9b1b3c4ec16da1
ggaspar22 <andrijchornobajj@gmail.com>
2021-12-06 10:39:16 +0200

    added hw 18

---
+++ zavdannia/script.js
@@ -0,0 +1,80 @@
+// Завдання 1. forEach
+// let arr = [5, 6, 7, 8, 9];
+
+// let number = 0;
+// arr.forEach(element =>  {
+//     number+=element;
+// });
+// console.log(number);
+
+// Завдання 2. Map
+// let arr = [5, 6, 7, 8, 9];
+// let second_arr = arr.map(value => value*value);
+// console.log(second_arr);
+
+// Завдання 3. Every
+
+// let arr = [{
+//     name: 'Ivan',
+//     country: 'Ukraine'
+// },
+// {
+//     name: 'Petro',
+//     country: 'Ukraine'
+// },
+// {
+//     name: 'Miguel',
+//     country: 'Cuba'
+// }
+// ]
+
+// let some_arr = arr.every(count => count.country ==='Ukraine');
+// console.log(some_arr);
+
+// Завдання 4. Some
+// let arr = [{
+//     name: 'Ivan',
+//     country: 'Ukraine'
+// },
+// {
+//     name: 'Petro',
+//     country: 'Ukraine'
+// },
+// {
+//     name: 'Miguel',
+//     country: 'Cuba'
+// }
+// ]
+
+// let some_arr = arr.some(count => count.country ==='Cuba');
+// console.log(some_arr);
+
+// Завдання 5. Filter
+// let arr = [1, 'string', [3, 4], 5, [6, 7]];
+// let some_arr = arr.filter(value =>  value!=parseInt(value)&& typeof value != 'string');
+// console.log(some_arr);
+
+// Завдання 6. Reduce
+
+// let arr = [1, 2, 5, 0, 4, 5, 6];
+
+
+
+
+//Завдання 7. Любим методом
+
+// let arr = [1, -2, 3, 0, 4, -5, 6, -11];
+
+// let arr_new = arr.filter(function (number) {
+//     if(number>0){
+//        return number;
+//     }
+// })
+
+// let to_stepen = arr_new.map(value => Math.sqrt(value));
+// console.log(to_stepen);
+
+
+
+
+
